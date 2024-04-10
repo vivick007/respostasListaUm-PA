b617ff7d0ecf75b72d03e6c3e1da864080c34f71
Ingrid Vitória <166644735+vivick007@users.noreply.github.com>
2024-04-10 16:43:31 -0300

    Create Circulo.java

---
+++ Circulo.java
@@ -0,0 +1,23 @@
+public class Circulo {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+}
