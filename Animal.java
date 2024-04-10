7970432ea38d069602d011935e2449edecf99a81
Ingrid Vitória <166644735+vivick007@users.noreply.github.com>
2024-04-10 16:39:40 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,27 @@
+abstract class Animal {
+    private String nome;
+    private String tipo;
+
+    public Animal(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public abstract void emitirSom();
+}
