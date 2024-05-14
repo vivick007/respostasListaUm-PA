62537019895c1ff1d80779e5afe9f26727002263
Ingrid Vitória <166644735+vivick007@users.noreply.github.com>
2024-05-14 16:26:34 -0300

    Create Cachorro.java

---
+++ Cachorro.java
@@ -0,0 +1,39 @@
+public class Cachorro {
+    private String nome;
+    private String raca;
+    private int idade;
+
+    public Cachorro(String nome, String raca, int idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String latir() {
+        return "Au au!";
+    }
+}
