6b67417fb51b025c2ab3b0087868b3c384343885
Ingrid Vitória <166644735+vivick007@users.noreply.github.com>
2024-05-14 16:23:48 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,35 @@
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private Data dataNascimento; 
+
+    public Cliente(String nome, String cpf, Data dataNascimento) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Data getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Data dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
